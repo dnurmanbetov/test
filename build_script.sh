e2150e811a8640fcc34bf2194220d8eee703613a
Daniiar Nurmanbetov <d.nurmanbetov@sxope.com>
2023-08-25 16:58:37 +0600

    test

--- build_script.sh
+++ build_script.sh
@@ -2,6 +2,11 @@
 git config --global user.email "dnurmanbetov@sxope.com"
 git config --global user.name "dnurmanbetov"
 
+git remote -v
+
+git remote set-url origin git@github.com:dnurmanbetov/test.git
+git remote -v
+
 echo "tesst" > test.txt
 git add .
 git commit -a -m "test"
